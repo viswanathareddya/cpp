215f73329771b73fe21cbca56a864483f7466343
viswanathareddya <63454456+viswanathareddya@users.noreply.github.com>
2020-09-05 11:47:22 +0530

    Update largest_resctangle.cpp

--- largest_resctangle.cpp
+++ largest_resctangle.cpp
@@ -108,6 +108,114 @@ vector<string> split_string(string input_string) {
     return splits;
 }
 
-/* optimized soltion */
+/* optimized soltion O(n)*/
 
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+/*
+long area_c(stack<int> indexes_of_height,int topindex,vector<int> h,int i ){
+    topindex=indexes_of_height.top();
+    indexes_of_height.pop();
+    long area=h[topindex]*(indexes_of_height.empty()? i:i-indexes_of_height.top()-1);
+
+    return area;
+}*/
+// Complete the largestRectangle function below.
+long largestRectangle(vector<int> h) {
+    stack<int> indexes_of_height;
+    int topindex;
+    int max_area=0;
+    int i=0;
+    int n=h.size();
+    while(i<n){  //passng trought hte heights
+        if(indexes_of_height.empty() || h[indexes_of_height.top()]<=h[i]){
+            //checking if the current bar is greater or equal height than the height of the bar at top index, 
+            //if so insert te index into stack of indexes
+            indexes_of_height.push(i);
+            i++;
+        }
+        else{
+            //if height is less than the height of the topindexes elment height
+            // take the top index height and calulate the area of that height                       //considering the current index as the right most edge and previous index as the left most edge for the considered height.
+        topindex=indexes_of_height.top();
+        indexes_of_height.pop();
+        long area=h[topindex]*(indexes_of_height.empty()? i:i-indexes_of_height.top()-1);
+        if(max_area<area){
+            max_area=area;
+            }
+        }
+    }
+    while(!indexes_of_height.empty()){
+        // Now calculate the areas for the remaining indexes from top of stack untill stack is empty . i.e., by end of stack we would have calculates areas possible for all different heights.
+        topindex=indexes_of_height.top();
+        indexes_of_height.pop();
+        long area=h[topindex]*(indexes_of_height.empty()? i:i-indexes_of_height.top()-1);
+        if(max_area<area){
+            max_area=area;
+        }
+    }
+    return max_area;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    string h_temp_temp;
+    getline(cin, h_temp_temp);
+
+    vector<string> h_temp = split_string(h_temp_temp);
+
+    vector<int> h(n);
+
+    for (int i = 0; i < n; i++) {
+        int h_item = stoi(h_temp[i]);
+
+        h[i] = h_item;
+    }
+
+    long result = largestRectangle(h);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
 
